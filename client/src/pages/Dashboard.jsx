771030a12a27f8ff781e2753d3d5a46cf3674729
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-04-17 23:14:35 -0700

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,33 @@
 import { Box } from '@mui/material';
+import { auth, db } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-    return (
-        <div className={`h-screen bg-[#0b2431] text-white`}>
+    const [data, setData] = useState(undefined);
 
+    useEffect(() => {
+        function getData() {
+            const docRef = doc(db, "users", auth.currentUser.uid);
+            getDoc(docRef).then((snap) => {
+                setData(snap.data());
+            });
+        }
+
+        getData();
+    });
+
+    return (
+        <div className={`h-screen bg-[#0b2431] px-10 py-10`}>
+            <div className={`mb-4 flex justify-between w-11/12 mx-auto`}>
+                <div className={`p-4 bg-white w-[24%] rounded-lg`}>
+                    <h2 className={`text-lg font-semibold text-gray-500 mb-1`}>Total Profits</h2>
+                    <h2 className={`text-2xl font-semibold text-black mb-3`}>{data ? "$" + data.earnings.toFixed(2) : "--"}</h2>
+                </div>
+                <div className={`p-4 bg-white w-[24%] rounded-lg`}></div>
+                <div className={`p-4 bg-white w-[24%] rounded-lg`}></div>
+                <div className={`p-4 bg-white w-[24%] rounded-lg`}></div>
+            </div>
         </div>
     )
 }
